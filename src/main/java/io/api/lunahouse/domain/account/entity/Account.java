884c7c47c74b11efffebb557d129f763c1536412
noel <yschoi@lunasoft.co.kr>
2020-09-25 16:07:00 +0900

    [2020-09-25]LunaHouseAPI#1 : Add Account Domain
     - 계정 생성에 필요한 기본 정보 및 이벤트 상태 변경 시 알림을 위한 수신 여부 항목

---
+++ src/main/java/io/api/lunahouse/domain/account/entity/Account.java
@@ -0,0 +1,62 @@
+package io.api.lunahouse.domain.account.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    // Email
+    @Column(unique = true)
+    private String email;
+
+    // 영어이름
+    @Column(unique = true)
+    private String engName;
+
+    // 비밀번호
+    private String password;
+
+    // 이메일 인증 여부
+    private boolean emailVerified;
+
+    // 이메일 검증 시 사용할 토큰
+    private String emailCheckToken;
+
+    // 가입일자
+    private LocalDateTime joinedAt;
+    
+    // 상태 메세지
+    private String statusMessage;
+
+    // 프로필 이미지
+    @Lob // String Type의 기본 Column은 varchar(255)로 설정되므로, 이보다 길어질 수 있는 URL항목은 @Lob을 이용 하여 Text로 Mapping 되도록 설정
+    @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    // 이벤트 생성 시 이메일 수신 여부
+    private boolean eventCreatedByEmail;
+
+    // 이벤트 생성 시 웹 수신 여부
+    private boolean eventCreatedByWeb;
+
+    // 이벤트 등록 시 이메일 수신 여부
+    private boolean eventEnrollmentResultByEmail;
+
+    // 이벤트 등록 시 웹 수신 여부
+    private boolean eventEnrollmentResultByWeb;
+
+    // 이벤트 갱신 시 이메일 수신 여부
+    private boolean eventUpdatedResultByEmail;
+
+    // 이벤트 갱신 시 웹 수신 여부
+    private boolean eventUpdatedResultByWeb;
+
+}
