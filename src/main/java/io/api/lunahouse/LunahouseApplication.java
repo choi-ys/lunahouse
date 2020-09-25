d96fa727a97b224dad2dc0701f1fe29e644956dc
noel <yschoi@lunasoft.co.kr>
2020-09-25 15:19:24 +0900

    [2020-09-25] Project init

---
+++ src/main/java/io/api/lunahouse/LunahouseApplication.java
@@ -0,0 +1,13 @@
+package io.api.lunahouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunahouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunahouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/api/lunahouse/LunahouseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.api.lunahouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunahouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
