35e070c046ed8eb49bb1ad48f38833954b841685
Noel <yschoi@lunasoft.co.kr>
2020-10-05 10:02:52 +0900

    [2020-10-05]LunaHouse#10 : 인증이 완료된 현재 사용자 정보 참조
     - 요청 시 @AuthenticationPrincipal를 이용한 사용자 인증 여부 확인
     - Spring Security의 User객체를 Account객체로 변환하기 위한 Adapter 구현
     - Adapter Class를 이용하여 현재 사용자 정보의 인증 정보 참조

---
+++ src/main/java/io/api/lunahouse/controller/main/MainController.java
@@ -0,0 +1,19 @@
+package io.api.lunahouse.controller.main;
+
+import io.api.lunahouse.domain.account.entity.Account;
+import io.api.lunahouse.util.annotation.CurrentUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(@CurrentUser Account account, Model model){
+        if(account != null){
+            model.addAttribute(account);
+        }
+        return "index";
+    }
+}
