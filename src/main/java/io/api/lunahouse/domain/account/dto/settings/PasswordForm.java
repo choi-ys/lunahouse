8bd41da83f49aafe11f5b483c324a9196ba60134
choi-ys <rcn115@naver.com>
2020-10-16 23:59:30 +0900

    [2020-10-16]LunaHouse#22 : 사용자 비밀번호 수정 Test
     - 입력값이 없는 비밀번호 수정 Test
     - 입력값이 잘못된 비밀번호 수정 Test
     - 입력값이 정상인 비밀번호 수정 Test

--- src/main/java/io/api/lunahouse/domain/account/dto/settings/PasswordForm.java
+++ src/main/java/io/api/lunahouse/domain/account/dto/settings/PasswordForm.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotNull;
+
 @Getter @Setter
 public class PasswordForm {
 
+    @NotNull
     @Length(min = 8, max = 50)
     private String newPassword;
 
+    @NotNull
     @Length(min = 8, max = 50)
     private String newPasswordConfirm;
 }
